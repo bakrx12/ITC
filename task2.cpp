00385e5bc9625f1cb0d1db65e5bc59b3962b8f42
AbuBakr <140571818+bakrx12@users.noreply.github.com>
2026-04-18 16:09:14 +0500

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "=============== Personal Information ==================" << endl;
+    cout << "1. Full Name: Abeeha Yousaf" << endl;
+    cout << "2. Registration No: L1S26BSCS0065" << endl;
+    cout << "3. Intermediate Program: ICS" << endl;
+    cout << "4. Hobbies: Reading, Programming, Gaming" << endl;
+    cout << "5. Goal in Life: Software Engineer with creative UI/UX" << endl;
+    cout << "6. Study Habits: I can do 3 hours of continuous study" << endl;
+    cout << "7. Last Movie Watched: Call Me By Your Name" << endl;
+    cout << "   Last Book Read: Atomic Habits, Perfect Mentor" << endl;
+    cout << "   Social Media Used: Instagram, Discord, Reddit" << endl;
+    cout << "8. Favorite Subject: Computer Science, because I enjoy" << endl;
+    cout << "   problem solving and coding" << endl;
+    cout << "=======================================================" << endl;
+    return 0;
+}
+
+// cout iostreamm
