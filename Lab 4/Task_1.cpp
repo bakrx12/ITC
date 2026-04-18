e3ca9cfb0d80f586b55134cc140b87f0c3252a58
AbuBakr <140571818+bakrx12@users.noreply.github.com>
2026-04-18 16:23:31 +0500

    Create Task_1.cpp

---
+++ Lab 4/Task_1.cpp
@@ -0,0 +1,40 @@
+//bismillah hirama nirajeem
+//Lab 4 Task 1 by AbuBakr
+//Roll No. L1S26BSCS0052
+
+#include<iostream>
+using namespace std;
+
+//learning experience:  it was ez af 😎 
+//skills required:      variable declare and initialize and then replacing it's values
+
+int main()
+{
+
+    //aik integer variable ko initialize krwaya hai
+    int X = 30;
+    //do float variable ko initialize krwaya hai
+    float Y = 25.0;
+    float Z = 5.5;
+    //dubara do integers ko initialize krwaya hai
+    int P = 21.12;
+    int Q = 0.25;
+
+    //expression ki values ko upar vale variables me rakh rha hu 😋
+    //reason: kyunke hamein kaha gya hai 🤓👆
+    X = 2*3 - 3/6 + 123% 12 - 23 * 2- 2/3;
+    Y = 123/10 - 123% 10 + 123/100 - 123/50 - 123 % 50;
+    Z = 2 - 3* 14 - 90%21/123/12- 2% 354 % 5% 5/6;
+    P = 3*4%3*5%6/2 - 23 + 34%10 - 34 /10 - 234;
+    Q = 234 - 234/10 + 123% 12 + 345%10 - 99;
+
+    //ab me in values ko print krwane laga hu 💅
+    cout << "6a. 2*3 - 3/6 + 123% 12 - 23 * 2- 2/3 = " << X << endl;
+    cout << "b. 123/10 - 123% 10 + 123/100 - 123/50 - 123 % 50 = " << Y << endl;
+    cout << "c. 2 - 3* 14 - 90%21/123/12- 2% 354 % 5% 5/6 = " << Z << endl;
+    cout << "d. 3*4%3*5%6/2 - 23 + 34%10 - 34 /10 - 234 = " << P << endl;
+    cout << "e. 234 - 234/10 + 123% 12 + 345%10 - 99 = " << Q << endl;
+
+    //ikhtetam krdia 😔
+    return 0; 
+}
