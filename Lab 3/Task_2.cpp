d91e7cb62896ed6027cfbc39606a93d701666269
AbuBakr <140571818+bakrx12@users.noreply.github.com>
2026-04-18 16:56:38 +0500

    Operator Precedence in C++

---
+++ Lab 3/Task_2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Operator Precedence Class Activity \n";
+    cout << "1. " << -2 - 3/4 + 7%4 - 2*4 + 4%3 - 7 * 3 + 4/7 << endl;
+    cout << "2. " << 9 + 10/4 + 10%4 - 5*4 + 10%3 - 3* 3 + 7/4 << endl;
+    cout << "3. " << - 13/4 - 9%5 + 3*3 + 12%3 - 4 * 3 + 7%7 - 2*3/4%7 - 10 << endl;
+    cout << "4. "<< - 2 *3/4%7%4 - 2*4/4%3 - 7 * 3%4/7 - 8 << endl;
+    cout << "5. "<< - 12/10 - 104%4 + 75/10 - 2*4 + 44%10 - 3 * 3 + 1234/100 << endl;
+    return 0;
+}
