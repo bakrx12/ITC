d315600df14275ae1abe7367eda2557d5a0af646
bakr <140571818+bakrx12@users.noreply.github.com>
2026-05-27 20:45:12 +0500

    Create Problem_4.cpp

---
+++ ITC Assignments/Assignment 3/Problem_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Vegetation Required: "
+         << (10000.0 / 500) * 100 << " kg" << endl;
+
+    cout << "Area Radius Required: "
+         << (10000.0 / 500) * 100 << " meters" << endl;
+
+    return 0;
+}
