d1fc3eff11ada1989e61f76995059b516a65507a
bakr <140571818+bakrx12@users.noreply.github.com>
2026-05-27 20:12:01 +0500

    Add Task_3.cpp with calculations and output

---
+++ ITC Assignments/Assignment 1/Task_3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "a. " << 30*20 << ", " << 30*4 << endl;
+    cout << "b. " << (30*20)*5.5 << endl;
+    cout << "c. " << (100)*25 << endl;
+    cout << "d. " << (((30*20) - (22*5*5/7)) * 11) / 2 << endl;
+    cout << "e. " << (100 - 2)*25 << endl;
+}
