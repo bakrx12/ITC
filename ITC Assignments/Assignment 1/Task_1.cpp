31cc4d75ca7fe4f2cd453f49ec8be1c387770721
bakr <140571818+bakrx12@users.noreply.github.com>
2026-05-27 20:11:16 +0500

    Implement personal information display in Task_1.cpp
    The program displays personal information including name, discipline, hobbies, goals, study habits, favorite subjects, and an interesting fact.

---
+++ ITC Assignments/Assignment 1/Task_1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+/* Write Complete C++ Programs for the following Problems (please don’t use any variables)
+1. Display the following information on the screen: (Please first repeat the following titles and then the
+answer e.g. Full Name: Ahmad Usman )
+a. Full Name
+b. Intermediate Discipline one of ICS, MEDICAL or PRE-ENG
+c. Hobbies
+d. Goal in Life
+e. Study Habits: Hours You can do Continuous Study
+f. Favorite Subject and Why?
+g. Tell Something Interesting About Yourself */
+
+int main()
+{
+    cout << "Full Name: AbuBakr Aslam" << endl;
+    cout << "Intermediate Discipline: ICS" << endl;
+    cout << "Hobbies: Reading, Learning, Watching" << endl;
+    cout << "Goal in Life: Pioneer UI/UX, Become good problem solver" << endl;
+    cout << "Study Habbits: I can continiously study for 2 hours" << endl;
+    cout << "Favourite Subjects: Discrete Structure, and ITC" <<endl;
+    cout << "Reason: Because, it's my favourite core concepts in cs" << endl;
+    cout << "Tell me something interesting: I support hitler!" << endl;
+}
