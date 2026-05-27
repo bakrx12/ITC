5dc675d41ae964e72a84c294ac793b8223288fa6
bakr <140571818+bakrx12@users.noreply.github.com>
2026-05-27 21:34:00 +0500

    Create Set_02_Problem_01.cpp

---
+++ ITC Assignments/Assignment 5/Set_02_Problem_01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+/*
+Write a complete C++ program to read 10 characters from the user, count and display number of lowercase
+English characters, number of uppercase English characters and number of digit characters
+For example: if user entered these values: 3a B q 4 Q 6 x 9 U the program should display the following:
+Number of lowercase English characters is 3
+Number of uppercase English characters is 3
+Number of digit English characters is 4
+*/
