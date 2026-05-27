a73994b7baa3121268e42ac544ac64aedef64700
bakr <140571818+bakrx12@users.noreply.github.com>
2026-05-27 20:11:43 +0500

    Add Task_2.cpp for house profit calculation
    Fix calculation of profit percentage in Task_2.cpp.

---
+++ ITC Assignments/Assignment 1/Task_2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "A house was bought of Rs 25 million" << endl;
+    cout << "The house is beign sold for 30 Million" << endl;
+    cout << "Profit: " << 30000000 - 25000000/(25000000*100) << endl;
+}
