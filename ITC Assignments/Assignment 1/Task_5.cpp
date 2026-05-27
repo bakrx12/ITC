bbd5bc1ae7469c62d5aaf116ad33e7fda161df30
bakr <140571818+bakrx12@users.noreply.github.com>
2026-05-27 20:12:32 +0500

    Add Task_5.cpp with volume calculation

---
+++ ITC Assignments/Assignment 1/Task_5.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "a. volume: " << 5*5*5 << "cm^3" << endl;
+    cout << "b. No given data" << endl;
+    cout << "c. " << ((0.5*0.5*0.5)/2) << " days" << endl;
+}
