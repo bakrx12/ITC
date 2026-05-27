929c6dd525a3e4f5b6de11da0f2466f904b6d37f
bakr <140571818+bakrx12@users.noreply.github.com>
2026-05-27 20:47:43 +0500

    Create Problem_7.cpp

---
+++ ITC Assignments/Assignment 3/Problem_7.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float kingKongHeight =0.0;
+    float kingKongWeight =0.0;
+
+    float gorillaHeight =0.0;
+    float gorillaWeight =0.0;
+
+    float gorillaFood=0.0;
+    float gorillaArea=0.0;
+
+    cout << "Enter King Kong height: ";
+    cin >> kingKongHeight;
+
+    cout << "Enter King Kong weight: ";
+    cin >> kingKongWeight;
+
+    cout << "Enter Gorilla height: ";
+    cin >> gorillaHeight;
+
+    cout << "Enter Gorilla weight: ";
+    cin >> gorillaWeight;
+
+    cout << "Enter Gorilla food per day: ";
+    cin >> gorillaFood;
+
+    cout << "Enter Gorilla area radius: ";
+    cin >> gorillaArea;
+
+    float vegetationRequired =0.0;
+    float areaRequired =0.0;
+
+    vegetationRequired =
+        (kingKongWeight / gorillaWeight) *
+        gorillaFood;
+
+    areaRequired =
+        (kingKongWeight / gorillaWeight) *
+        gorillaArea;
+
+    cout << "Vegetation Required: "
+         << vegetationRequired << endl;
+
+    cout << "Area Required: "
+         << areaRequired << endl;
+
+    return 0;
+}
