0e4a320428bcc4d688cab01f1aea85d6083d28f2
bakr <140571818+bakrx12@users.noreply.github.com>
2026-05-27 20:44:44 +0500

    Create Problem_3.cpp

---
+++ ITC Assignments/Assignment 3/Problem_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Surface Area of Cube: "
+         << 6 * 0.25 * 0.25 << endl;
+
+    cout << "Surface Area of Cylinder: "
+         << (44 / 7.0) * 0.25 * 0.25 * 0.25 << endl;
+
+    cout << "Surface Area of Sphere: "
+         << (88 / 7.0) * 0.25 * 0.25 * 0.25 << endl;
+
+    return 0;
+}
